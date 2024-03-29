cd00223e14b53cd1b803ceb1b45e99820158192b
“Ziyad <ziyadclombe.pro@gmail.com>
2024-03-29 14:46:26 +0100

    Commentaires et nettoyage du code

--- slider infini/assets/js/script.js
+++ slider infini/assets/js/script.js
@@ -4,12 +4,13 @@ let arrowRight = document.querySelector('.fa-arrow-right');
 let shuffle = document.querySelector('.fa-shuffle');
 let img = document.querySelector('.img');
 
-
+// Array d'images
 const arrayImg = ['images/1.jpg', 'images/2.jpg', 'images/3.jpg', 'images/4.jpg', 'images/5.jpg', 'images/6.jpg'];
 
 let i = 0;
 let clickCount = 0;
 
+// Fonction pour passer à l'image suivante
 arrowRight.addEventListener('click', () => {
     i += 1;
     if (i == arrayImg.length) {
@@ -18,6 +19,7 @@ arrowRight.addEventListener('click', () => {
     img.src = arrayImg[i];
 });
 
+// Fonction pour passer à l'image précédente
 arrowLeft.addEventListener('click', () => {
     if (i == 0) {
         i = 6;
@@ -26,23 +28,7 @@ arrowLeft.addEventListener('click', () => {
     img.src = arrayImg[i];
 });
 
-
-
-// let previousNumber = i;
-// shuffle.addEventListener('click', () => {
-//     console.log(i);
-//     i = Math.floor(Math.random() * arrayImg.length);
-//     img.src = arrayImg[i];
-//     console.log(i);
-//     previousNumber = i;
-//     if(previousNumber == i){
-//         i = Math.floor(Math.random() * arrayImg.length);
-//         console.log('hiihihi');
-//         console.log(previousNumber);
-//     }
-// });
-
-// Faire en sorte que ca ne tombe jamais sur la meme image dans le random
+// Fonction pour passer à une image aléatoire
 shuffle.addEventListener('click', () => {
     let last = img.src.slice(-13);
     console.log('last : ' + last);
@@ -61,6 +47,7 @@ shuffle.addEventListener('click', () => {
 
 
 let timer;
+// Fonction pour mettre en pause/marche le slider
 pause.addEventListener('click', () => {
     clickCount++
     console.log(clickCount);
